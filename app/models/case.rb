5108e0b1b4f177e8f4486b2645bd114b8b176d3c
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2022-03-14 16:28:44 +0100

    stated associations

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,4 +1,16 @@
 class Case < ApplicationRecord
   belongs_to :patient
   belongs_to :doctor
+
+  validates :type, inclusion: { in: ['lab', 'usg', 'digital xray', 'outsource lab', 'ecg', 'ct scan']}
+
+  TYPE_OPTIONS = [
+    ['LAB', 'lab'],
+    ['USG', 'usg'],
+    ['DIGITAL XRAY', 'digital xray'],
+    ['XRAY', 'xray'],
+    ['OUTSOURCE LAB', 'outsource lab'],
+    ['ECG', 'ecg'],
+    ['CT SCAN', 'ct scan']
+  ]
 end
