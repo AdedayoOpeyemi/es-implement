3513cefc5f62d9fff5f7ba681dddf414e27df9f6
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2022-03-15 08:48:38 +0100

    Added benchmark timer for seeding patient

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+def seed_patients
+  10_000.times do
+    new_patient = Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+    puts new_patient.first_name + " " + new_patient.last_name
+  end
+end
+
+def timespent
+  time = Benchmark.measure do
+    yield
+  end
+  puts "time spent #{time.real.round(2)} seconds."
+end
+
+timespent {seed_patients}
