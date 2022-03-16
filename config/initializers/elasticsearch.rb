c4388f0a1bf58cd58fb2651b7ebb0337574f515c
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2022-03-16 15:11:09 +0100

    added pointer to cluster

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,5 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new({
-  log: true
-})
+# Elasticsearch::Model.client = Elasticsearch::Client.new({
+#   log: true
+# })
+
+Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
