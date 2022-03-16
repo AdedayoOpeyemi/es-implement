d91e32dd0f240cfdd4867319cb3f14e432a5d906
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2022-03-16 14:59:16 +0100

    add conditional for render in patient controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,18 +1,25 @@
 class PatientsController < ApplicationController
   def index
-    response = Patient.__elasticsearch__.search(
-      query: {
-        multi_match: {
-          query: params[:query],
-          fields: ['first_name', 'last_name']
-        }
+
+    if params[:query].blank? 
+      render json: {
+        message: "Provide parameter"
       }
-    ).records
-  
-    render json: {
-      results: response.results,
-      total: response.total
-    }
+    else
+      response = Patient.__elasticsearch__.search(
+        query: {
+          multi_match: {
+            query: params[:query],
+            fields: ['first_name', 'last_name']
+          }
+        }
+      ).records
+    
+      render json: {
+        results: response.results,
+        total: response.total
+      }
+    end
   end
 end
 
